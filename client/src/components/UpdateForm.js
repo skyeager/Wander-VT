aaabc6392d089e3bd3cf02050de5d000da6b74d4
Sarah Yeager <yeager.sarah.k@gmail.com>
2022-10-25 12:21:25 -0400

    update form CSS added

--- client/src/components/UpdateForm.js
+++ client/src/components/UpdateForm.js
@@ -1,6 +1,13 @@
-//need to update hike state, reference delete in Instance
+const UpdateForm = () => {
+  const handleChange = (event) => {
+    setFormState({ ...formState, [event.target.id]: event.target.value })
+  }
 
-//update form component
+  return <div className="update-form"></div>
+}
+
+export default UpdateForm
+
 //conditionally render when click update to show update form
 //have words show up with input fields with starting state value=formstate (object of key value pairs to pass to backend) of instance (not blank initial state)
 //handle change the same
